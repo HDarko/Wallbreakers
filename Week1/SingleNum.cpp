307e5137e4ce9a70469be23c70c7dbc0fbce95ed
HD <37189335+HDarko@users.noreply.github.com>
2019-06-25 20:32:21 -0400

    Update and rename SingleNum to SingleNum.cpp

--- Week1/SingleNum.cpp
+++ Week1/SingleNum.cpp
@@ -1,6 +1,13 @@
 /*
+Problem
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 In order to find the single non repeating num in a vector on intergers.
 
+Note:
 Here is a trick:
  If you XOR a bit and 0 then you get 1
 but XOR the same two bits and you get 0
