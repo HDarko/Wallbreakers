f785920ad37abce6ce4c95458e8b7d10bd9a1462
HD <37189335+HDarko@users.noreply.github.com>
2019-06-25 20:35:00 -0400

    Update and rename TwoSum to TwoSum.cpp

--- Week1/TwoSum.cpp
+++ Week1/TwoSum.cpp
@@ -1,5 +1,12 @@
 /*
 
+Problem:
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Notes:
+
 num[x] +num[y]= target
 thus num [y] -target =num[x]
 Best way to do this is to use a hash map
